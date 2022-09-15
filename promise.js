3455b7461c6275b4ae76042933ef88f527012df0
Igit Purwahyudi <igithks@gmail.com>
2022-09-16 04:52:18 +0700

    promise sudah dimodifikasi oleh REA4 - Igit Purwahyudi

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
+
+
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (input) => {
+  let comment = 0;
+
+  const data = await Promise.all([
+    promiseTheaterIXX(), 
+    promiseTheaterVGC()]
+  );
+
+  data.forEach(element => {
+    element.forEach(dataBioskop => {
+      if (dataBioskop.hasil == input){
+        comment++;
+      }      
+    })  
+  });
+  
+  return comment;
+
+};
 
 module.exports = {
   promiseOutput,
